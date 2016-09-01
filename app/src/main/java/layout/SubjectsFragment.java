b721035098fcaaf3bd2131d0790a5686d9fa1a1d
Rigoberto Molina <rigothedev@gmail.com>
2016-08-31 19:15:55 -0700

    Getting data from JSON file working

--- app/src/main/java/layout/SubjectsFragment.java
+++ app/src/main/java/layout/SubjectsFragment.java
@@ -20,7 +20,12 @@ import tech.rigo.pi.SignUpActivity;
 import tech.rigo.pi.Subject;
 import tech.rigo.pi.SubjectsAdapter;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.*;
 
 public class SubjectsFragment extends Fragment {
     public SubjectsFragment() {
@@ -29,26 +34,51 @@ public class SubjectsFragment extends Fragment {
     
     private ListView mListView;
     
+    public String loadJSONFromAsset() {
+        String json = null;
+        try {
+            InputStream is = getResources().openRawResource(R.raw.data);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_subjects, container, false);
 
         mListView = (ListView) rootView.findViewById(R.id.subjects_list_view);
-
-        Subject subject;
+        
         ArrayList<Subject> subjects = new ArrayList<Subject>();
         
-        // NOTE: Just a test subjects.
-        subject = new Subject();
-        subject.setName("Algebra 1");
-        subject.setFormulaCount(10);
-        subjects.add(subject);
-        
-        subject = new Subject();
-        subject.setName("Geometry");
-        subject.setFormulaCount(9);
-        subjects.add(subject);
+        try {
+            JSONObject obj = new JSONObject(loadJSONFromAsset());
+            JSONArray m_jArry = obj.getJSONArray("subjects");
+            Subject subject;
+            
+            for (int i = 0; i < m_jArry.length(); i++) {
+                JSONObject jo_inside = m_jArry.getJSONObject(i);
+                Log.d("Subject-->", jo_inside.getString("subject_name"));
+                String subject_name = jo_inside.getString("subject_name");
+                
+                //Add your values in your `ArrayList` as below:
+                subject = new Subject();
+                subject.setName(subject_name);
+                subject.setFormulaCount(10); // Test
+                
+                subjects.add(subject);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         
         mListView.setAdapter(new SubjectsAdapter(getActivity(), subjects));
         
